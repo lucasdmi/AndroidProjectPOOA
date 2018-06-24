baecdc53d78d806c46a66ebe46a3cf5241a04d0f
Lucas <lucasmisidoro@gmail.com>
2018-06-24 09:48:32 -0300

    Implementando a listView

--- app/src/main/java/br/edu/iff/pooa20181/androidproject/activity/PrimeiraActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/androidproject/activity/PrimeiraActivity.java
@@ -1,15 +1,44 @@
 package br.edu.iff.pooa20181.androidproject.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import pooa20181.iff.edu.br.androidprojectpooa.R;
 
-public class PrimeiraActivity extends AppCompatActivity {
+public class PrimeiraActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+    private String[] activities = {"ListaEventoActivity"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primeira);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, activities);
+
+                ListView listView = (ListView) findViewById(R.id.listaMenu);
+                listView.setAdapter(adapter);
+                listView.setOnItemClickListener(this);
+
+    }
+
+
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        Intent intent = null;
+
+                      try {
+                          Class obj = Class.forName ("br.edu.iff.pooa20181.androidproject.activity."+activities[position]);
+                               intent = new Intent(this, obj);
+
+                      } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                        }
+
+                        startActivity(intent);
+
     }
 }
