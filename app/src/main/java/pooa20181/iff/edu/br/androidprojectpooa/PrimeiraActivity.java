a318eaa570de64925383721d1c489397ece9ce14
Lucas <lucasmisidoro@gmail.com>
2018-06-24 08:46:47 -0300

    Initial commit

---
+++ app/src/main/java/pooa20181/iff/edu/br/androidprojectpooa/PrimeiraActivity.java
@@ -0,0 +1,13 @@
+package pooa20181.iff.edu.br.androidprojectpooa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrimeiraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primeira);
+    }
+}
