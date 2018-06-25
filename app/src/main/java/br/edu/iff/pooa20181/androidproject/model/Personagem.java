f6097bb845720a2aac78c396dc4ff1af23da82e3
Lucas <lucasmisidoro@gmail.com>
2018-06-25 17:16:55 -0300

    Crianco a classe personagem

---
+++ app/src/main/java/br/edu/iff/pooa20181/androidproject/model/Personagem.java
@@ -0,0 +1,71 @@
+package br.edu.iff.pooa20181.androidproject.model;
+
+public class Personagem {
+    private int id;
+    private String nome;
+    private String classe;
+    private String raca;
+    private int nivel;
+
+
+
+    public Personagem(){
+
+    }
+
+    public Personagem(int id, String nome, String  classe)
+    {
+        this.id = id;
+        this.nome = nome;
+        this.classe = classe;
+        this.raca = raca;
+        this.nivel = nivel;
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+
+
+
+}
