8f47cc2b163ec8947bfc43c2454103f217b6858c
Lucas <lucasmisidoro@gmail.com>
2018-06-25 20:39:09 -0300

    Criação do layout personagem detalhe

---
+++ app/src/main/java/br/edu/iff/pooa20181/androidproject/activity/PersonagemDetalhe.java
@@ -0,0 +1,15 @@
+package br.edu.iff.pooa20181.androidproject.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pooa20181.iff.edu.br.androidprojectpooa.R;
+
+public class PersonagemDetalhe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personagem_detalhe);
+    }
+}
