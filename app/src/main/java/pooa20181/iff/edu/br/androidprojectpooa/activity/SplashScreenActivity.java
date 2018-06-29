f5348361dc8493f34c2ebcbb3738a10416e1487c
Lucas <lucasmisidoro@gmail.com>
2018-06-29 12:12:25 -0300

    Refatorção da Activity Splash Screen

--- app/src/main/java/pooa20181/iff/edu/br/androidprojectpooa/activity/SplashScreenActivity.java
+++ app/src/main/java/pooa20181/iff/edu/br/androidprojectpooa/activity/SplashScreenActivity.java
@@ -1,5 +1,7 @@
 package pooa20181.iff.edu.br.androidprojectpooa.activity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,10 +9,26 @@ import pooa20181.iff.edu.br.androidprojectpooa.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 2500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMain();
+            }
+        }, SPLASH_TIME_OUT);
 
     }
+    private void mostrarMain()
+    {
+        Intent intent = new Intent(SplashScreenActivity.this, Principal.class);
+        startActivity(intent);
+        finish();
+    }
 }
+
